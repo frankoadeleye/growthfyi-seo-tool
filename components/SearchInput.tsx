5c8818c5283fb8f53519688798676453973a39df
FrankNZYMT <frank.adeleye@nzymtechnologies.com>
2023-09-15 19:34:59 +0100

    feat: updated placeholder for search input

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -10,7 +10,7 @@ function SearchInput(searchProps: SearchInputProps) {
       className="search md:w-96 w-64 m-auto text-white bg-transparent h-16 focus:outline-none appearance-none text-md leading-6 placeholder-slate-white rounded-none py-2 pl-5 ring-1 ring-slate-200"
       type="text"
       aria-label="Enter any url"
-      placeholder="Enter any url..."
+      placeholder="Enter any url... and hit ENTER"
       value={url}
       onChange={onChange}
       {...otherProps}
